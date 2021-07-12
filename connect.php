eac4e8c99a9b90378dd8a9f8a5d46c7daf183ba2
mercy-un <70755686+mercy-un@users.noreply.github.com>
2021-07-12 16:02:04 +0100

    first commit

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php date_default_timezone_set("Africa/Lagos");
 $date=date("d M Y, h:ia");
-$con = mysqli_connect("localhost","root","","attendance");
+$cleardb_db = substr($cleardb_url["path"],1);
+$con = mysqli_connect("//b3d3c7f1a9ca6a:1018c88b@us-cdbr-east-04.cleardb.com/heroku_cd88e6887f5cf7b?reconnect=true","b3d3c7f1a9ca6a","1018c88b","cleardb_db");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
