dbd5ffed718d70bad69fe67da95f92bc95a004cc
mercy-un <70755686+mercy-un@users.noreply.github.com>
2021-07-13 17:16:05 +0100

    test

--- connect.php
+++ connect.php
@@ -1,6 +1,21 @@
 <?php date_default_timezone_set("Africa/Lagos");
 $date=date("d M Y, h:ia");
-$con = mysqli_connect("kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","iinnyuxdwgusikfv:j4u57gdety96mp1i","j4u57gdety96mp1i","jawsdb-rugged-85760");
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection was successfully established!";
+/*$con = mysqli_connect("kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","iinnyuxdwgusikfv:j4u57gdety96mp1i","j4u57gdety96mp1i","jawsdb-rugged-85760");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
